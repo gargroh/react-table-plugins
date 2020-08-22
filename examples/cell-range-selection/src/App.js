c86925c21759f0c760d19871a332f5b680b2677e
Harish Kulkarni <07harish96@gmail.com>
2020-08-22 18:29:45 +0530

    feat(useCellRangeSelection) Add scrolling on edges while selecting cells in an example

--- examples/cell-range-selection/src/App.js
+++ examples/cell-range-selection/src/App.js
@@ -1,8 +1,14 @@
-import React from "react";
-import styled from "styled-components";
-import { useTable } from "react-table";
-import { useCellRangeSelection } from "react-table-plugins";
-import makeData from "./makeData";
+import React from 'react'
+import styled from 'styled-components'
+import { useTable } from 'react-table'
+import { useCellRangeSelection } from 'react-table-plugins'
+// useScrollOnEdges: Ability to scroll on edges while you're selecting cells
+// Note: You are free to include any external lib for this usecase
+import useScrollOnEdges from 'react-scroll-on-edges'
+
+import makeData from './makeData'
+// Usage Component is for Demo purpose
+import Usage from './Usage'
 
 const Styles = styled.div`
   padding: 1rem;
@@ -37,9 +43,9 @@ const Styles = styled.div`
   pre {
     float: left;
   }
-`;
+`
 
-function Table({ columns, data }) {
+function Table ({ columns, data }) {
   // Use the state and functions returned from useTable to build your UI
   const {
     getTableProps,
@@ -49,51 +55,66 @@ function Table({ columns, data }) {
     prepareRow,
     // currentSelectedCellIds: cells of a current selected range
     // selectedCellIds: All previously selected cells
-    state: { selectedCellIds, currentSelectedCellIds },
+    state: { selectedCellIds, currentSelectedCellIds, isSelectingCells },
     // getCellsBetweenId (Fn): Pass two cell Ids to get all cell Ids between them
     getCellsBetweenId,
     setSelectedCellIds,
-    cellsById,
+    cellsById
   } = useTable(
     {
       columns,
       data,
       // cellIdSplitBy (string): Cell id is split by column.id + cellIdSplitBy + row.id
-      cellIdSplitBy: "cols_rows",
+      cellIdSplitBy: 'cols_rows',
       initialState: {
-        selectedCellIds: {},
-      },
+        selectedCellIds: {}
+      }
     },
     useCellRangeSelection
-  );
+  )
 
-  let cellsSelected = { ...currentSelectedCellIds, ...selectedCellIds };
+  let cellsSelected = { ...currentSelectedCellIds, ...selectedCellIds }
 
   // returns two random cell ids, this is just for the demo.
   const getRandomCellIds = React.useCallback(() => {
-    let cloneCellIds = Object.keys(cellsById);
+    let cloneCellIds = Object.keys(cellsById)
     let randomCellId = () =>
-      cloneCellIds[(cloneCellIds.length * Math.random()) << 0];
-    return [randomCellId(), randomCellId()];
-  }, [cellsById]);
+      cloneCellIds[(cloneCellIds.length * Math.random()) << 0]
+    return [randomCellId(), randomCellId()]
+  }, [cellsById])
 
   // getCellsBetweenId returns all cell Ids between two cell Id, and then setState for selectedCellIds
   const selectRandomCells = React.useCallback(() => {
-    const cellsBetween = getCellsBetweenId(...getRandomCellIds());
-    setSelectedCellIds(cellsBetween);
-  }, [getCellsBetweenId, setSelectedCellIds, getRandomCellIds]);
+    const cellsBetween = getCellsBetweenId(...getRandomCellIds())
+    setSelectedCellIds(cellsBetween)
+  }, [getCellsBetweenId, setSelectedCellIds, getRandomCellIds])
+
+  // 'useScrollOnEdges' hook helps us when we have height or width set to the table
+  // and when user is selecting cells and is near any of the edges, table will scroll by itself.
+  const getEdgeScrollingProps = useScrollOnEdges({
+    canAnimate: isSelectingCells // Scroll when user `isSelectingCells` is True
+    // scrollSpeed: 15, -> Optional, default is 12,
+    // edgeSize: 30     -> Optional, default is 25
+  })
 
   return (
     <>
       <button onClick={selectRandomCells}>Select cells randomly</button>
-      <div>
+      <Usage></Usage>
+      <div
+        {...getEdgeScrollingProps({
+          style: {
+            height: '400px'
+          }
+        })}
+      >
         <table {...getTableProps()}>
           <thead>
-            {headerGroups.map((headerGroup) => (
+            {headerGroups.map(headerGroup => (
               <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column) => (
+                {headerGroup.headers.map(column => (
                   <th {...column.getHeaderProps()}>
-                    {column.render("Header")}
+                    {column.render('Header')}
                   </th>
                 ))}
               </tr>
@@ -101,91 +122,87 @@ function Table({ columns, data }) {
           </thead>
           <tbody {...getTableBodyProps()}>
             {rows.map((row, i) => {
-              prepareRow(row);
+              prepareRow(row)
               return (
                 <tr {...row.getRowProps()}>
-                  {row.cells.map((cell) => {
+                  {row.cells.map(cell => {
                     return (
                       <td
                         {...cell.getCellRangeSelectionProps()}
                         {...cell.getCellProps()}
                         style={
                           cellsSelected[cell.id]
-                            ? { backgroundColor: "#6beba8", userSelect: "none" }
-                            : { backgroundColor: "white", userSelect: "none" }
+                            ? { backgroundColor: '#6beba8', userSelect: 'none' }
+                            : { backgroundColor: 'white', userSelect: 'none' }
                         }
                       >
-                        {cell.render("Cell")}
+                        {cell.render('Cell')}
                       </td>
-                    );
+                    )
                   })}
                 </tr>
-              );
+              )
             })}
           </tbody>
         </table>
-        <pre>
-          <code>
-            {JSON.stringify(
-              { selectedCellIds, currentSelectedCellIds },
-              null,
-              2
-            )}
-          </code>
-        </pre>
       </div>
+      <pre>
+        <code>
+          {JSON.stringify({ selectedCellIds, currentSelectedCellIds }, null, 2)}
+        </code>
+      </pre>
     </>
-  );
+  )
 }
 
-function App() {
+function App () {
   const columns = React.useMemo(
     () => [
       {
-        Header: "Name1",
+        Header: 'Name1',
         columns: [
           {
-            Header: "First Name",
-            accessor: "firstName",
+            Header: 'First Name',
+            accessor: 'firstName'
           },
           {
-            Header: "Last Name",
-            accessor: "lastName",
-          },
-        ],
+            Header: 'Last Name',
+            accessor: 'lastName'
+          }
+        ]
       },
       {
-        Header: "Info",
+        Header: 'Info',
         columns: [
           {
-            Header: "Age",
-            accessor: "age",
+            Header: 'Age',
+            accessor: 'age'
           },
           {
-            Header: "Visits",
-            accessor: "visits",
+            Header: 'Visits',
+            accessor: 'visits'
           },
           {
-            Header: "Status",
-            accessor: "status",
+            Header: 'Status',
+            accessor: 'status'
           },
           {
-            Header: "Profile Progress",
-            accessor: "progress",
-          },
-        ],
-      },
+            Header: 'Profile Progress',
+            accessor: 'progress'
+          }
+        ]
+      }
     ],
     []
-  );
+  )
 
-  const data = React.useMemo(() => makeData(20), []);
+  const data = React.useMemo(() => makeData(20), [])
 
   return (
     <Styles>
       <Table columns={columns} data={data} />
     </Styles>
-  );
+  )
 }
 
-export default App;
+export default App
